0b956f00b10bdc3142c83fba1f0cd5d47e7f6588
Taylor S Kendall <taylorkendall@gmail.com>
2022-02-02 23:04:04 -0800

    Branding, licsencing and format changes. Added logo.

---
+++ src/components/nmf.svelte
@@ -0,0 +1,12 @@
+<script>
+	let src = '/img/NFM-logo-no-background.gif';
+	let name = 'NFM';
+    
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+	<slot />
+</svg>
+
+<!--<a href="https://seattlefarmersmarkets.org">-->
+<img {src} alt="{name}" >
